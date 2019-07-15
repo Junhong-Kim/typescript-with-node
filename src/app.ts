492487577d48c457c4d0e736778a4ca798023e73
Junhong-Kim <jhk1005jhk@gmail.com>
2019-07-15 22:55:32 +0900

    추가: HTTP request logger middleware

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import logger from "morgan";
 import routes from "./routes";
 
 const app = express();
+
+app.use(logger("dev"));
 
 app.set("port", process.env.PORT || 3000);
 app.use("/", routes);
